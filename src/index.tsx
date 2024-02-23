cbf648164eab9a9c06f964447006b035d47e2c37
Vamba45 <vambafool@bk.ru>
2024-02-23 14:13:50 +0300

    add create-month func

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-
-import Heading from './components/Heading.jsx'; 
-import Footer from './components/Footer'; 
 import { BrowserRouter as Router, Route, Routes, Link }from 'react-router-dom';
 
 import './styles/style.scss';
-import { Provider } from 'react-redux';
-import  {setupStore} from './store/store';
-
-const store = setupStore();
+import { createDate } from './utils/helpers/date/createDate';
+import { createMonth } from './utils/helpers/date/createMonth';
 
 const rootNode: HTMLElement | any = document.getElementById("app");    // элемент для рендеринга приложения React
 // получаем корневой элемент 
 const root = ReactDOM.createRoot(rootNode);
 // рендеринг в корневой элемент
+
+console.log(createDate());
+console.log(createMonth());
+
 root.render(
-    <Provider store={store}>
-        <Router>
-            <Routes>
-                <Route path='/' element={
-                    <Heading/>
-                } />
-                <Route path='/footer' element={<Footer/>}/>
-            </Routes>
-        </Router>
-    </Provider>
+    <Router>
+        <Routes>
+        </Routes>
+    </Router>
 );
---
+++ src/utils/helpers/date/createMonth.ts
@@ -0,0 +1,39 @@
+import { createDate } from "./createDate";
+import { getMonthNumberOfDays } from "./getMonthNumberOfDays";
+import { getWeekNumber } from "./getWeekNumber";
+
+interface CreateMonthsParams {
+    locale?: string;
+    date?: Date;
+}
+
+export const createMonth = (params?: CreateMonthsParams) => {
+    const date = params?.date ?? new Date();
+    const locale = params?.locale ?? 'default';
+
+    const d = createDate({date, locale})
+    const {month: monthName, year, monthNumber, monthIndex} = d
+
+    const getDay = (dayNumber: number) => {
+        return createDate({date: new Date(year, monthIndex, dayNumber), locale})
+    }
+
+    const createMonthDays = () => {
+        const days = []
+
+        for(let i = 0; i <= getMonthNumberOfDays(monthIndex, year) - 1; i += 1) {
+            days[i] = getDay(i + 1)
+        }
+
+        return days;
+    }
+
+    return {
+        getDay,
+        monthName,
+        monthIndex,
+        monthNumber,
+        year,
+        createMonthDays   
+    }
+}
